610dd23d5dbcc7a51657f7b1a0c76cb05f156268
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-05-29 16:06:11 +0530

    Helper Functions and Unit Tests, step 3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,7 +8,26 @@ const totalLikes = (blogs) => {
   }, 0);
 };
 
+const favouriteBlog = (blog) => {
+  if (blog.length > 0) {
+    let blogWithMostLikes = null;
+    let likes = 0;
+
+    blog.forEach((blog) => {
+      if (blog.likes > likes) {
+        likes = blog.likes;
+        blogWithMostLikes = blog;
+      }
+    });
+
+    return blogWithMostLikes;
+  }
+
+  return 'none';
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favouriteBlog,
 };
