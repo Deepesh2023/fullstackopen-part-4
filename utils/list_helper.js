087ecca6cb4931569f89832fd17daee96f663740
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-05-29 15:32:45 +0530

    Helper Functions and Unit Tests, step 2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return blogs ? 1 : 0;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => {
+    return (sum += blog.likes);
+  }, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
